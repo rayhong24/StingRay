d68129cc53ebd30a7c9870590f0918d272ed6898
Ray Hong <rayhong24@gmail.com>
2017-11-12 22:33:54 -0800

    [Made a list to represent the chess board]

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print('yellow chess.comrades')
+import numpy as np
+board =[["r","n","b","q","k","b","n","r"],
+        ["p","p","p","p","p","p","p","p"],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        ["P","P","P","P","P","P","P","P"],
+        ["R","N","B","Q","K","B","N","R"]]
 
+def print_board(board):
+    print(board)
+
+print_board(board)
