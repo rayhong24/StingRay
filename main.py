b887117f686db39bf381e013d4000bfc4d707fbb
Ray Hong <rayhong24@gmail.com>
2017-11-13 23:02:06 -0800

    Added a way to return the piece at the given coordinate

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+letter2index = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
+    "h": 7
+}
 board = np.matrix([["r", "n", "b", "q", "k", "b", "n", "r"],
                    ["p","p","p","p","p","p","p","p"],
                    [" "," "," "," "," "," "," "," "],
@@ -7,9 +17,29 @@ board = np.matrix([["r", "n", "b", "q", "k", "b", "n", "r"],
                    [" "," "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "," "],
                    ["P","P","P","P","P","P","P","P"],
-                   ["R", "N", "B", "Q", "K", "B", "N", "R"][)
+                   ["R", "N", "B", "Q", "K", "B", "N", "R"]])
 
 def print_board(board):
     print(board)
 
+
+def query_board(pos):
+    """return the piece at the given coordinate"""
+    row, col = chess_to_matrix(pos)
+    print(board[row, col])
+
+
+def chess_to_matrix(pos):
+    """convert chess notaion to matrix notation"""
+    row = 8 - int(pos[1])
+    col = letter2index.get(pos[0])
+    return row, col
+
+
+def pawn_move(move):
+    if query_board(move[1] - 1) == P
+
+
+
 print_board(board)
+query_board("e8")
